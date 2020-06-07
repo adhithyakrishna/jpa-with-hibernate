887daa60e8ae580498f901e0600e6ba1fcbdad52
Adhithyakrishna <akovaisr@buffalo.edu>
2020-06-06 23:44:45 -0400

    A branch to learn the advanced JPA concepts and reorganising the folder structure

--- 02-jpa-advanced/src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Course.java
+++ 02-jpa-advanced/src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Course.java
@@ -25,53 +25,27 @@ import org.slf4j.LoggerFactory;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@NamedQueries(value = { 
-		@NamedQuery(name = "query_get_all_courses", 
-				query = "Select  c  From Course c"),		
-		@NamedQuery(name = "query_get_all_courses_join_fetch", 
-		query = "Select  c  From Course c JOIN FETCH c.students s"),		
-		@NamedQuery(name = "query_get_100_Step_courses", 
-		query = "Select  c  From Course c where name like '%100 Steps'") })
-@Cacheable
-@SQLDelete(sql="update course set is_deleted=true where id=?")
-@Where(clause="is_deleted = false")
 public class Course {
-
-	private static Logger LOGGER = LoggerFactory.getLogger(Course.class);
-	
 	@Id
 	@GeneratedValue
 	private Long id;
 
-	@Column(nullable = false)
 	private String name;
 
-	@OneToMany(mappedBy="course")
-	private List<Review> reviews = new ArrayList<>();
-	
-	@ManyToMany(mappedBy="courses")
-	@JsonIgnore
-	private List<Student> students = new ArrayList<>();
-	
-	@UpdateTimestamp
-	private LocalDateTime lastUpdatedDate;
-
-	@CreationTimestamp
-	private LocalDateTime createdDate;
-	
-	private boolean isDeleted;
-	
-	@PreRemove
-	private void preRemove(){
-		LOGGER.info("Setting isDeleted to True");
-		this.isDeleted = true;
-	}
-
+	/*
+	 * JPA expects no arg constructor, mandated by jpa protect because, Other
+	 * classes that do not inherit from course will not make use of the constructor
+	 */
 	protected Course() {
+
 	}
 
 	public Course(String name) {
 		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
 	}
 
 	public String getName() {
@@ -82,33 +56,4 @@ public class Course {
 		this.name = name;
 	}
 
-	
-	public List<Review> getReviews() {
-		return reviews;
-	}
-
-	public void addReview(Review review) {
-		this.reviews.add(review);
-	}
-
-	public void removeReview(Review review) {
-		this.reviews.remove(review);
-	}
-
-	public List<Student> getStudents() {
-		return students;
-	}
-
-	public void addStudent(Student student) {
-		this.students.add(student);
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	@Override
-	public String toString() {
-		return String.format("Course[%s]", name);
-	}
 }
